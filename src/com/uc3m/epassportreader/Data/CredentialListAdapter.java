a4492b3b60787d3bf17e04712f83948ad206abcb
Alximw <alximw@gmail.com>
2013-11-16 12:00:42 +0100

    Added Credential's list custom adapter. Changed API Level to 14. Remove "Add Credential" button.

--- src/com/uc3m/epassportreader/Data/CredentialListAdapter.java
+++ src/com/uc3m/epassportreader/Data/CredentialListAdapter.java
@@ -2,17 +2,68 @@ package com.uc3m.epassportreader.Data;
 
 import java.util.ArrayList;
 
+import com.uc3m.epassportreader.R;
+
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 public class CredentialListAdapter extends ArrayAdapter<Credentials> {
 
 	//list of credentials shown on the list
 	private ArrayList<Credentials> list;
+	private Context adapterContext;
+	int layout;
+	private static class ListElementHolder{
+		TextView eDate;
+		TextView bDate;
+		TextView ID;
+		
+	}
+	
 	
 	public CredentialListAdapter(Context context, int layout) {
 		super(context, layout);
 		list=new ArrayList<Credentials>();
+		adapterContext=context;
+
+
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View listElement=convertView;
+		ListElementHolder elementHolder;
+		//TODO: Create Holder class
+		
+		
+		if(listElement==null){
+			//ListElement has not been previously created
+			LayoutInflater inflater=(LayoutInflater) adapterContext.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			listElement=inflater.inflate(layout,null,false);
+			elementHolder=new ListElementHolder();
+			
+			elementHolder.bDate=(TextView)listElement.findViewById(R.id.credential_bDate);
+			elementHolder.eDate=(TextView)listElement.findViewById(R.id.credential_eDate);
+			elementHolder.ID=(TextView)listElement.findViewById(R.id.credential_ID);
+			
+			listElement.setTag(elementHolder);
+
+
+		}else{
+			//ListElement has been previously created
+			elementHolder=(ListElementHolder)listElement.getTag();
+		}
+		
+		elementHolder.bDate.setText(list.get(position).getBirthDate().toString() );
+		elementHolder.eDate.setText(list.get(position).getExpiryDate().toString() );
+		elementHolder.bDate.setText(list.get(position).getePassportID());
+		
+		
+		return listElement;	
 	}
 
 }
--- src/com/uc3m/epassportreader/GUI/CredentialChooser.java
+++ src/com/uc3m/epassportreader/GUI/CredentialChooser.java
@@ -1,19 +1,14 @@
 package com.uc3m.epassportreader.GUI;
 
 import com.uc3m.epassportreader.R;
-import com.uc3m.epassportreader.R.id;
-import com.uc3m.epassportreader.R.layout;
 import com.uc3m.epassportreader.Data.CredentialListAdapter;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 
-public class CredentialChooser extends Activity implements OnClickListener, OnLongClickListener{
+public class CredentialChooser extends  Activity {
 
 Button add;				
 ListView credentialList;
@@ -26,28 +21,16 @@ ListView credentialList;
 		setContentView(R.layout.main_layout);
 		
 		//Initialize the view elements
-		add=(Button)findViewById(R.id.main_addCredentialsButton);
 		credentialList=(ListView)findViewById(R.id.main_credentialsList);
 		
 		//create new listAdapter (custom) and set it as credential list adapter
-		CredentialListAdapter adapter=new CredentialListAdapter(getApplicationContext(), R.layout.credentiallayout);
+		CredentialListAdapter adapter=new CredentialListAdapter(getApplicationContext(),R.layout.credentiallayout);
 		credentialList.setAdapter(adapter);
 	}
 
 
 
-	@Override
-	public void onClick(View v) {
-		//When list element is Clicked
-	}
-
-
-
-	@Override
-	public boolean onLongClick(View v) {
-		//When list element is longClicked
-		return false;
-	}
+	
 
 	
 
